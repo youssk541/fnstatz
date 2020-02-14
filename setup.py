a5c720ae1020d3bb6805827e6055f0a37a29c8ec
youssk541 <50636385+youssk541@users.noreply.github.com>
2020-02-14 23:22:06 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import codecs
+
+setup(
+    name='PFE',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/youssk541/fnstatz',
+    license='Licence',
+    author='Youssef berbiche',
+    author_email='berbiche.youssef@gmail.com',
+    description='Twitter notifications for financial markets'
+)
